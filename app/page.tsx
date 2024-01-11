68578ba7579982ca8b22189761633896f255943e
faizan3165 <faizanmehmood3165@gmail.com>
2024-01-11 19:07:44 +0500

    complete homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
+import { Poppins } from "next/font/google";
+
 import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+import LoginBtn from "@/components/auth/LoginBtn";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 const Home = () => {
   return (
-    <>
-      <div className="font-semibold text-emerald-400 text-9xl">Home</div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
 
-      <Button size={"lg"} variant={"destructive"} className="ml-10 rounded-xl">
-        Click Me!
-      </Button>
-    </>
+        <p className="text-white text-lg">A simple Authentication Service</p>
+
+        <div>
+          <LoginBtn>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign In
+            </Button>
+          </LoginBtn>
+        </div>
+      </div>
+    </main>
   );
 };
 
